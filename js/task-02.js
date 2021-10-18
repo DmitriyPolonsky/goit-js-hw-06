6182881f88c2f4179da5c0754096ad8139f25c0d
DmitriyPolonsky <dipol.cg@gmail.com>
2021-10-18 19:28:39 +0300

    task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const igredientsList = document.querySelector('#ingredients')
+
+ingredients.forEach(ingredient => {
+  const listEl = document.createElement('li');
+  listEl.textContent = ingredient;
+  listEl.classList.add('item');
+  igredientsList.append(listEl);
+
+  console.log(listEl);
+});
