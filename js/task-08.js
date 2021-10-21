7aaf012aa33c0c2d7c19cde71453fe09481be481
DmitriyPolonsky <dipol.cg@gmail.com>
2021-10-21 23:38:34 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', formSubmit);
 
+function formSubmit (event) {
+    event.preventDefault();
+    if (
+        event.currentTarget.elements.email.value === '' ||
+        event.currentTarget.elements.password.value === '') {
+        alert ('Все поля должны быть заполнены');
+    } else {
+        const userInfo = {
+            email: event.currentTarget.elements.email.value,
+            password: event.currentTarget.elements.password.value,
+        };
+        console.log(userInfo);
+        event.currentTarget.reset();
+          }
+ };
